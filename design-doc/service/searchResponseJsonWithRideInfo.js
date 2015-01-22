d04f3568c6a654a00f0459049c50d297a93f2f72
neearjkukreti <neeraj.kr.kukreti@gmail.com>
2015-01-22 10:12:34 +0530

    Sample Search Service Output
    Sample Search Service output Json shared by Anubhav

---
+++ design-doc/service/searchResponseJsonWithRideInfo.js
@@ -0,0 +1,57 @@
+{
+    "basic": {
+        "SourceCity": "Delhi",
+        "DestinationCity": "Dehradun City",
+        "Distance": "255.90",
+        "Unit": "KM",
+        "TimeinMinutes": "285",
+        "AverageFare": "650",
+        "Currency": "Rupees",
+        "Ridedate": "24/01/2015"
+    },
+    "totalAvailabilty": 23,
+    "showResults": 5,
+    "pageNo": 1,
+    "availability": {
+        "2001": {
+            "Name": "Dilaram R. Singh",
+            "ShortDescription": "I am a IT professional,working as manager",
+            "Age": "34",
+            "Timein24format": "13:30:00",
+            "ProfilePic": "http://rideashare.com/images/user/images2001.jpg",
+            "facebookid": "100010001001"
+        },
+        "2002": {
+            "Name": "Radhe L. Singh",
+            "ShortDescription": "I am a Woodland professional,working as Bootmaker",
+            "Age": "24",
+            "Timein24format": "10:00:00",
+            "ProfilePic": "http://rideashare.com/images/user/images2002.jpg",
+            "facebookid": "100010001111"
+        },
+        "2013": {
+            "Name": "Rakshat kumar",
+            "ShortDescription": "Shop owner",
+            "Age": "38",
+            "Timein24format": "12:00:00",
+            "ProfilePic": "http://rideashare.com/images/user/images2013.jpg",
+            "facebookid": "100010001341"
+        },
+        "2019": {
+            "Name": "Mohd. RafiQ",
+            "ShortDescription": "Flower BoutiQue",
+            "Age": "41",
+            "Timein24format": "09:00:00",
+            "ProfilePic": "http://rideashare.com/images/user/images2019.jpg",
+            "facebookid": "1009010001341"
+        },
+        "2102": {
+            "Name": "George Hawkins",
+            "ShortDescription": "Motor Cyclist",
+            "Age": "31",
+            "Timein24format": "11:00:00",
+            "ProfilePic": "http://rideashare.com/images/user/images2102.jpg",
+            "facebookid": "100016001341"
+        }
+    }
+}
